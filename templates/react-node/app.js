bb50c65c9007e696303e60543849a5cdeec903c1
Sudarshan shinde <shindesudarshan070@gmail.com>
2021-05-12 10:22:54 +0530

    react-node template done

--- templates/react-node/app.js
+++ templates/react-node/app.js
@@ -3,32 +3,60 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressStaticGzip = require('express-static-gzip')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('dotenv').config()
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// webpack setup
+if (process.env.NODE_ENV === "development") {
+  var webpack = require("webpack");
+  var webpackConfig = require("./webpack.config");
+  var compiler = webpack(webpackConfig);
+
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+    })
+  );
+  app.use(require("webpack-hot-middleware")(compiler));
+}
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  "/dist/bundle",
+  expressStaticGzip(path.join(__dirname, "dist/bundle"), {
+    enableBrotli: true,
+    orderPreference: ["br", "gz"],
+    setHeaders: function (res, path) {
+      res.setHeader("Cache-Control", "public, max-age=31536000");
+    },
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ templates/react-node/client/index.js
@@ -0,0 +1,8 @@
+import { render } from 'react-dom'
+import React from 'react'
+import App from './App'
+import '../public/stylesheets/style.scss'
+
+
+
+render(<App />, document.getElementById('root'))
